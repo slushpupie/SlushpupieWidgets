2cf242e688bf961d654445e7b6c79517f3f09664
Jay Kline <jay@slushpupie.com>
2012-08-17 15:29:57 -0500

    Fix namespaces for ADT-17 and newer

--- src/com/slushpupie/widget/SeekBarPreference.java
+++ src/com/slushpupie/widget/SeekBarPreference.java
@@ -19,7 +19,9 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
   private static final String androidns = "http://schemas.android.com/apk/res/android";
   //When an app imports a library, the namespace becomes that of the app. Since we cant know it ahead of
   //time, we are going to rely on the string resources to provide the name. Ugly hack. 
-  //private static final String appns = "http://schemas.android.com/apk/res/com.slushpupie.livewallpaper";
+  //private static final String appns = "http://schemas.android.com/apk/res/com.slushpupie.widget";
+  //As of ADT-17 you can use res-auto
+  private static final String appns = "http://schemas.android.com/apk/res-auto";
   
   private SeekBar mSeekBar;
   private TextView mSplashText, mValueText;
@@ -30,7 +32,7 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
 
   public SeekBarPreference(Context context, AttributeSet attrs) {
     super(context, attrs);
-    String appns = context.getString(R.string.package_ns);
+    //String appns = context.getString(R.string.package_ns);
     mContext = context;
 
     int res = attrs.getAttributeResourceValue(androidns, "dialogMessage", -1);
